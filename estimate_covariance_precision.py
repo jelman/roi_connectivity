a34c599f6c7ca6eae7aefd7df3ad689fc5a73649
Jeremy Elman <jelman@lbl.gov>
2013-09-18 16:12:49 -0700

    add function to save out group results

--- estimate_covariance_precision.py
+++ estimate_covariance_precision.py
@@ -8,6 +8,11 @@ import pandas as pd
 from scipy.stats import zscore
 from sklearn import covariance
 
+def save_group_data(data_dict, outfile):
+    data_df = pd.DataFrame(data_dict)
+    sorted_df = data_df.sort_index()
+    sorted_df.to_csv(outfile, sep=',', header=False, index=False)
+    
 
 def estimate_sub(tc_array):
     estimator.fit(tc_array)
@@ -21,6 +26,11 @@ def run_subject(subtc_file):
     sub_cv1D = utils.flat_triu(sub_cv)
     sub_precision1D = utils.flat_triu(sub_precision)
     return sub_cv1D, sub_precision1D
+
+
+
+if __name__ == '__main__':
+
 
 #### Set parameters #######
 datadir = '/home/jagust/rsfmri_ica/CPAC/connectivity/timecourses'
@@ -37,10 +47,16 @@ group_precision = {}   #Create empty dataframe to hold group data
 
 for subtc_file in tc_files:
     subid = utils.get_subid(subtc_file)
-    subject_order.append(subid)   
     sub_cv, sub_precision = run_subject(subtc_file)
     group_cv[subid] = sub_cv
     group_precision[subid] = sub_precision
-    cv_outfile = os.path.join(outdir, ''.join(['Covariance_',outname,'.csv']))
+    
+cv_outfile = os.path.join(outdir, ''.join(['Covariance_',outname,'.csv']))
+save_group_data(group_cv, cv_outfile)
+precision_outfile = os.path.join(outdir, ''.join(['Precision_',outname,'.csv']))
+save_group_data(group_precision, precision_outfile)
+
+#TO DO:
+# Add capability to only include timecourses of ROIs contained in list
 # Search for pattern contained in list    
 #[col for net in nets for col in subtc_zscored.columns if net in col]
