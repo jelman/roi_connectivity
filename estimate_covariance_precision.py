97b15f06bbd4dcc896eab2a7eaad88a6e8c21d81
Jeremy Elman <jelman@lbl.gov>
2013-09-18 14:45:01 -0700

    add function to estimate covariance and precision matrices for subject

--- estimate_covariance_precision.py
+++ estimate_covariance_precision.py
@@ -9,6 +9,15 @@ from scipy.stats import zscore
 from sklearn import covariance
 
 
+def estimate_sub(tc_array):
+    estimator.fit(tc_array)
+    return estimator.covariance_, estimator.precision_
+
+
+def run_subject(subtc_file):
+    subtc_df = pd.read_csv(subtc_file, sep=None)
+    subtc_zscored = pd.DataFrame(zscore(subtc_df), columns=subtc_df.columns)
+    sub_cv, sub_precision = estimate_sub(subtc_zscored)
 
 
 #### Set parameters #######
@@ -20,4 +29,6 @@ tc_files = glob(os.path.join(datadir, tc_glob))
 tc_files.sort()
 ###########################
 
+for subtc_file in tc_files:
+    subid = utils.get_subid(subtc_file)
 
