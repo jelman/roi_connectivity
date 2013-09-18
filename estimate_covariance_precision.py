eff78416aab9f5c87cdaf2e53995a4b8cf807285
Jeremy Elman <jelman@lbl.gov>
2013-09-18 15:51:23 -0700

    append subject matrices to group dict

--- estimate_covariance_precision.py
+++ estimate_covariance_precision.py
@@ -18,7 +18,9 @@ def run_subject(subtc_file):
     subtc_df = pd.read_csv(subtc_file, sep=None)
     subtc_zscored = pd.DataFrame(zscore(subtc_df), columns=subtc_df.columns)
     sub_cv, sub_precision = estimate_sub(subtc_zscored)
-
+    sub_cv1D = utils.flat_triu(sub_cv)
+    sub_precision1D = utils.flat_triu(sub_precision)
+    return sub_cv1D, sub_precision1D
 
 #### Set parameters #######
 datadir = '/home/jagust/rsfmri_ica/CPAC/connectivity/timecourses'
@@ -29,6 +31,16 @@ tc_files = glob(os.path.join(datadir, tc_glob))
 tc_files.sort()
 ###########################
 
+
+group_cv = {}   #Create empty dataframe to hold group data
+group_precision = {}   #Create empty dataframe to hold group data
+
 for subtc_file in tc_files:
     subid = utils.get_subid(subtc_file)
-
+    subject_order.append(subid)   
+    sub_cv, sub_precision = run_subject(subtc_file)
+    group_cv[subid] = sub_cv
+    group_precision[subid] = sub_precision
+    cv_outfile = os.path.join(outdir, ''.join(['Covariance_',outname,'.csv']))
+# Search for pattern contained in list    
+#[col for net in nets for col in subtc_zscored.columns if net in col]
