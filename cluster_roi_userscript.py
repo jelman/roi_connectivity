050803280b43e5a82af88e436df6b8cf1f5b16c0
Jeremy Elman <jelman@lbl.gov>
2013-09-19 12:55:48 -0700

    Add parameter definitions

--- cluster_roi_userscript.py
+++ cluster_roi_userscript.py
@@ -6,6 +6,17 @@ import make_local_connectivity_tcorr as make_tcorr
 
 
 
-
+    
+    ######### Set parameters #################################
+    ##########################################################
+    outdir = '/home/jagust/rsfmri_ica/cluster_roi'
+    subj_cluster_suffix = '_tcorr'
+    maskfile = '/home/jagust/rsfmri_ica/CPAC/rsfmriMask_3mm.nii.gz'
+    data_glob = '/home/jagust/rsfmri_ica/CPAC/pipeline_rsfmri/B*/functional_mni/_scan_func_B*_4d/_csf_threshold_0.96/_gm_threshold_0.7/_wm_threshold_0.96/_compcor_ncomponents_5_selector_pc10.linear1.wm1.global0.motion1.quadratic1.gm0.compcor0.csf1/_bandpass_freqs_0.01.0.08/bandpassed_demeaned_filtered_warp.nii.gz'
+    data_files = glob(data_glob)
+    thresh = .5
+    ##########################################################
+    
+    
 make_local_connectivity_tcorr( infile, maskfile, outfile, thresh )
 outname=outfile+'_'+str(k)+'.npy'	
