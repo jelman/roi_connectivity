dd515ae442a7f957a107744515ccb474adedcff3
Jeremy Elman <jelman@lbl.gov>
2013-09-19 13:31:08 -0700

    change to use update cluster_roi code and base on pyCluster_test.py template

--- cluster_roi_userscript.py
+++ cluster_roi_userscript.py
@@ -1,22 +1,38 @@
-import os, sys
+import sys
+sys.path.append("/home/jagust/jelman/CODE/cluster_roi")
+from make_local_connectivity_tcorr import *
+from binfile_parcellation import *
+from group_binfile_parcellation import *
+from make_image_from_bin import *
+from make_image_from_bin_renum import *
+from time import time
 from glob import glob
-import general_utilities as utils
-sys.path.append('/home/jagust/jelman/CODE/pyClusterROI')
-import make_local_connectivity_tcorr as make_tcorr
+
+
+# Directory to save output of clustering
+outdir = '/home/jagust/rsfmri_ica/cluster_roi'
+
+# the name of the maskfile that we will be using
+maskname='/home/jagust/rsfmri_ica/CPAC/rsfmriMask_3mm.nii.gz'
+
+# make a list of all of the input fMRI files that we will be using
+data_glob = '/home/jagust/rsfmri_ica/CPAC/pipeline_rsfmri/B*/functional_mni/_scan_func_B*_4d/_csf_threshold_0.96/_gm_threshold_0.7/_wm_threshold_0.96/_compcor_ncomponents_5_selector_pc10.linear1.wm1.global0.motion1.quadratic1.gm0.compcor0.csf1/_bandpass_freqs_0.01.0.08/bandpassed_demeaned_filtered_warp.nii.gz'
+infiles = glob(data_glob)
+    
+# set threshold for individual connectivity matrics (r>.5 used in the paper)
+thresh = .5
 
 
 
-    
-    ######### Set parameters #################################
-    ##########################################################
-    outdir = '/home/jagust/rsfmri_ica/cluster_roi'
-    subj_cluster_suffix = '_tcorr'
-    maskfile = '/home/jagust/rsfmri_ica/CPAC/rsfmriMask_3mm.nii.gz'
-    data_glob = '/home/jagust/rsfmri_ica/CPAC/pipeline_rsfmri/B*/functional_mni/_scan_func_B*_4d/_csf_threshold_0.96/_gm_threshold_0.7/_wm_threshold_0.96/_compcor_ncomponents_5_selector_pc10.linear1.wm1.global0.motion1.quadratic1.gm0.compcor0.csf1/_bandpass_freqs_0.01.0.08/bandpassed_demeaned_filtered_warp.nii.gz'
-    data_files = glob(data_glob)
-    thresh = .5
-    ##########################################################
-    
-    
-make_local_connectivity_tcorr( infile, maskfile, outfile, thresh )
-outname=outfile+'_'+str(k)+'.npy'	
+##### Step 1. Individual ConnEctivity Matrices 
+# construct the connectivity matrices using tcorr and a threshold
+
+for idx, in_file in enumerate(infiles):
+
+    # construct an output filename for this file
+    outname= os.path.join(outdir, 'rm_tcorr_conn_'+str(idx)+'.npy')
+
+    print 'tcorr connectivity',in_file
+    # call the funtion to make connectivity
+    make_local_connectivity_tcorr( in_file, maskname, outname, 0.5 )
+
