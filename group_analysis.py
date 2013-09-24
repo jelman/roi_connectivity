2e5fbfaf0d7883dad2e3edc72f3eda76a66fb033
Jeremy Elman <jelman@lbl.gov>
2013-09-24 11:13:13 -0700

    add if name == main statement to allow running from command line

--- group_analysis.py
+++ group_analysis.py
@@ -6,46 +6,48 @@ from glob import glob
 import numpy as np
 
 
-
-####################### Set parameters################
-basedir = '/home/jagust/rsfmri_ica/CPAC/connectivity'
-datadir = '/home/jagust/rsfmri_ica/CPAC/connectivity/matrices'
-nnodes = 39
-modeldir = '/home/jagust/rsfmri_ica/GIFT/models/Old'
-des_file = os.path.join(modeldir, 'Covariate_Old_log_demeaned.mat')
-con_file = os.path.join(modeldir, 'Covariate_Old_log_demeaned.con')
-resultsglob = '*MSDL_rois_0-01_0-08.csv'
-##############################################################
+if __name__ == '__main__':
 
 
-## Run group analysis with randomise
-####################################
-exists, resultsdir = gu.make_dir(basedir,'randomise') 
-result_files = glob(os.path.join(datadir,resultsglob))
-for data_file in result_files:
-    data = np.genfromtxt(data_file, names=None, dtype=float, delimiter=',')
-    pth, fname, ext = gu.split_filename(data_file)
-    img_fname = os.path.join(resultsdir, fname + '.nii.gz')
-    saveimg = gu.save_img(data, img_fname)
-    rand_basename = os.path.join(resultsdir, fname)
-    p_uncorr_list, p_corr_list = ga.randomise(saveimg, 
-                                                rand_basename, 
-                                                des_file, 
-                                                con_file)     
-    uncorr_results = ga.get_results(p_uncorr_list)
-    corr_results = ga.get_results(p_corr_list)
-           
-    fdr_results = {}
-    for i in range(len(uncorr_results.keys())):
-        conname = sorted(uncorr_results.keys())[i]
-        fdr_corr_arr = ga.multi_correct(uncorr_results[conname])
-        fdr_results[conname] = gu.square_from_combos(fdr_corr_arr, nnodes)
-        
-        outfile = os.path.join(resultsdir, 
-                            ''.join([rand_basename, '_fdr_corrp_','tstat',str(i+1),'.txt']))
-        # Save results to file
-        np.savetxt(outfile, 
-                    fdr_results[conname], 
-                    fmt='%1.5f', 
-                    delimiter='\t')  
-        print('Saved corrected output to %s'%(outfile))        
+    ####################### Set parameters################
+    basedir = '/home/jagust/rsfmri_ica/GIFT/GICA_d75/roi_connectivity/'
+    datadir = '/home/jagust/rsfmri_ica/GIFT/GICA_d75/roi_connectivity/matrices'
+    nnodes = 23
+    modeldir = '/home/jagust/rsfmri_ica/GIFT/models/Old'
+    des_file = os.path.join(modeldir, 'Covariate_Old_log_demeaned.mat')
+    con_file = os.path.join(modeldir, 'Covariate_Old_log_demeaned.con')
+    resultsglob = '*mancovan_preproc.csv'
+    ##############################################################
+
+
+    ## Run group analysis with randomise
+    ####################################
+    exists, resultsdir = gu.make_dir(basedir,'randomise') 
+    result_files = glob(os.path.join(datadir,resultsglob))
+    for data_file in result_files:
+        data = np.genfromtxt(data_file, names=None, dtype=float, delimiter=',')
+        pth, fname, ext = gu.split_filename(data_file)
+        img_fname = os.path.join(resultsdir, fname + '.nii.gz')
+        saveimg = gu.save_img(data, img_fname)
+        rand_basename = os.path.join(resultsdir, fname)
+        p_uncorr_list, p_corr_list = ga.randomise(saveimg, 
+                                                    rand_basename, 
+                                                    des_file, 
+                                                    con_file)     
+        uncorr_results = ga.get_results(p_uncorr_list)
+        corr_results = ga.get_results(p_corr_list)
+               
+        fdr_results = {}
+        for i in range(len(uncorr_results.keys())):
+            conname = sorted(uncorr_results.keys())[i]
+            fdr_corr_arr = ga.multi_correct(uncorr_results[conname])
+            fdr_results[conname] = gu.square_from_combos(fdr_corr_arr, nnodes)
+            
+            outfile = os.path.join(resultsdir, 
+                                ''.join([rand_basename, '_fdr_corrp_','tstat',str(i+1),'.txt']))
+            # Save results to file
+            np.savetxt(outfile, 
+                        fdr_results[conname], 
+                        fmt='%1.5f', 
+                        delimiter='\t')  
+            print('Saved corrected output to %s'%(outfile))        
