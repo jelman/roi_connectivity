0b70c1161406d97c008439cd3b06e24f3894da6a
Jeremy Elman <jelman@lbl.gov>
2013-09-26 12:22:21 -0700

    add function to estimate correlation and coherence

--- estimate_covariance_precision.py
+++ estimate_covariance_precision.py
@@ -7,6 +7,8 @@ import nibabel as nib
 import pandas as pd
 from scipy.stats import zscore
 from sklearn import covariance
+from nitime.timeseries import TimeSeries
+from nitime.analysis import CorrelationAnalyzer, CoherenceAnalyzer
 
 def save_group_data(data_dict, outfile):
     data_df = pd.DataFrame(data_dict)
@@ -16,65 +18,108 @@ def save_group_data(data_dict, outfile):
     return sorted_df
     
 
-def estimate_sub(tc_array):
-    estimator = covariance.GraphLassoCV(max_iter=500, verbose=True)
+def estimate_cov(tc_array):
+    estimator = covariance.GraphLassoCV(max_iter=1000, verbose=False)
     estimator.fit(tc_array)
     np.fill_diagonal(estimator.covariance_, 0)
     np.fill_diagonal(estimator.precision_, 0)    
     return estimator.covariance_, estimator.precision_
+    
+
+def estimate_corr_coh(tc_array, tr):
+    TR = tr
+    f_lb = 0.01
+    f_ub = 0.15
+    T = TimeSeries(tc_array.T, sampling_interval=TR)
+    T.metadata['roi'] = tc_array.columns
+    Corr = CorrelationAnalyzer(T)
+    Coh = CoherenceAnalyzer(T)
+    freq_idx = np.where((Coh.frequencies > f_lb) * \
+                        (Coh.frequencies < f_ub))[0]
+    return Corr.corrcoef, np.mean(Coh.coherence[:, :, freq_idx], -1)
 
 
-def run_subject(subtc_file, tc_subset=None):
+def run_subject(subtc_file, tr, tc_subset=None):
+    # Load data in dataframe
     subtc_df = pd.read_csv(subtc_file, sep=None)
+    # Take subset of ROIs
     if tc_subset:
-        col_subset = [col for net in tc_subset for col in subtc_df.columns if net in col]
+        col_subset = [col for net in tc_subset for col in subtc_df.columns \
+                        if net in col]
         subtc_df = subtc_df[col_subset]
+    # normalize time courses
     subtc_zscored = pd.DataFrame(zscore(subtc_df), columns=subtc_df.columns)
-    sub_cv, sub_precision = estimate_sub(subtc_zscored)
-    sub_cv_zscored = sub_cv = np.arctanh(sub_cv)
-    sub_precision_zscored = sub_cv = np.arctanh(sub_precision)
-    sub_cv1D = utils.flat_triu(sub_cv_zscored)
+    # get covariance and precision matrices using Graph Lasso L1 penalization
+    sub_cov, sub_precision = estimate_cov(subtc_zscored)
+    # get correlation and coherence matrices
+    sub_corr, sub_coh = estimate_corr_coh(subtc_zscored, tr)
+    # zscore correlation values
+    sub_cov_zscored = np.arctanh(sub_cov)
+    sub_precision_zscored = np.arctanh(sub_precision)
+    sub_corr_zscored = np.arctanh(sub_corr)
+    # flatten upper triangle into 1D vector
+    sub_cov1D = utils.flat_triu(sub_cov_zscored)
     sub_precision1D = utils.flat_triu(sub_precision_zscored)
-    return sub_cv1D, sub_precision1D
+    sub_corr1D = utils.flat_triu(sub_corr_zscored)
+    sub_coh1D = utils.flat_triu(sub_coh)
+    return sub_cov1D, sub_precision1D, sub_corr1D, sub_coh1D
 
 
 
-def main(datadir, outdir, outname, tc_files, tc_subset=None, subgroup=None):
-    group_cv = {}   #Create empty dataframe to hold group data
-    group_precision = {}   #Create empty dataframe to hold group data
+def main(datadir, outdir, outname, tc_files, tr, tc_subset=None, subgroup=None):
+    group_cov = {}   #Create empty dataframe to hold group covariance
+    group_precision = {}   #Create empty dataframe to hold group precision
+    group_corr = {} # Create empty dataframe to hold group correlation
+    group_coh = {} # Create empty dataframe to hold group coherence
     for subtc_file in tc_files:
         subid = utils.get_subid(subtc_file)
-        if subgroup and subid in subgroup:
-            sub_cv, sub_precision = run_subject(subtc_file, tc_subset)
-            group_cv[subid] = sub_cv
+        if not subgroup:
+            sub_cov, sub_precision, sub_corr, sub_coh = run_subject(subtc_file, 
+                                                                    tr, 
+                                                                    tc_subset)
+            group_cov[subid] = sub_cov
             group_precision[subid] = sub_precision
+            group_corr[subid] = sub_corr
+            group_coh[subid] = sub_coh
+        elif subgroup and subid in subgroup:
+            sub_cov, sub_precision, sub_corr, sub_coh = run_subject(subtc_file, 
+                                                                    tr, 
+                                                                    tc_subset)
+            group_cov[subid] = sub_cov
+            group_precision[subid] = sub_precision 
+            group_corr[subid] = sub_corr            
+            group_coh[subid] = sub_coh
         else:
-            sub_cv, sub_precision = run_subject(subtc_file, tc_subset)
-            group_cv[subid] = sub_cv
-            group_precision[subid] = sub_precision    
-    cv_outfile = os.path.join(outdir, ''.join(['Covariance_',outname,'.csv']))
-    group_cv_df = save_group_data(group_cv, cv_outfile)
+            continue   
+    cov_outfile = os.path.join(outdir, ''.join(['Covariance_',outname,'.csv']))
+    group_cov_df = save_group_data(group_cov, cov_outfile)
     precision_outfile = os.path.join(outdir, ''.join(['Precision_',outname,'.csv']))
     group_precision_df = save_group_data(group_precision, precision_outfile)
+    corr_outfile = os.path.join(outdir, ''.join(['Correlation_',outname,'.csv']))
+    group_corr_df = save_group_data(group_corr, corr_outfile)    
+    coh_outfile = os.path.join(outdir, ''.join(['Coherence_',outname,'.csv']))
+    group_coh_df = save_group_data(group_coh, coh_outfile)    
     
     
 if __name__ == '__main__':
 
 
     #### Set parameters #######
-    datadir = '/home/jagust/rsfmri_ica/CPAC/connectivity/timecourses/Greicius_90_rois'
-    outdir = '/home/jagust/rsfmri_ica/CPAC/connectivity/matrices'
+    datadir = '/home/jagust/rsfmri_ica/CPAC/connectivity/timecourses/smoothed/Greicius_90_rois'
+    outdir = '/home/jagust/rsfmri_ica/CPAC/connectivity/matrices/smoothed'
     outname = 'Greicius_90rois_0-01_0-08_subset'
     tc_glob = 'B*_timecourses.csv'
     tc_files = glob(os.path.join(datadir, tc_glob))
     tc_files.sort()
-    tc_subset = None
+    tc_subset = ['anterior_Salience','dDMN','LECN','post_Salience','Precuneus',\
+                'RECN','vDMN','Visuospatial']
     subgroup_file = None
+    tr = 2.2
     ###########################
     if subgroup_file:
         with open(subgroup_file) as f:
             subgroup = f.read().splitlines()
         
-    main(datadir, outdir, outname, tc_files, subgroup=None)
+    main(datadir, outdir, outname, tc_files, tr, tc_subset=None, subgroup=None)
 
 
